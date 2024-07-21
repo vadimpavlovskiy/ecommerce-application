e205d0ae770327b009fb7bf9a78ab20505d672c1
vadimpavlovskiy <vadimpavlovskiy@icloud.com>
2024-07-21 13:25:33 -0600

    Created a model and migration for a product

---
+++ backend/database/migrations/2024_07_21_191305_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('sku')->unique();
+            $table->integer('length');
+            $table->integer('width');
+            $table->integer('height');
+            $table->integer('sleeping_area_length');
+            $table->integer('sleeping_area_width');
+            $table->decimal('price', 8, 2);
+            $table->decimal('discounted_price', 8, 2)->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
