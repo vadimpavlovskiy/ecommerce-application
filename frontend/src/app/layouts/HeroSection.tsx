9c94123dcf15ed53c50539aee7e7e24ecf5b274e
vadimpavlovskiy <vadimpavlovskiy@icloud.com>
2024-07-21 12:04:21 -0600

    Created a hero section layout

---
+++ frontend/src/app/layouts/HeroSection.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import React from 'react'
+import { HeroSectionProps } from '../types/layoutTypes/HeroSection'
+
+import {Nunito} from 'next/font/google'
+import {Raleway} from 'next/font/google'
+
+const nunito = Nunito({ subsets: ['latin'], weight: ['900'] })
+const raleway = Raleway({ subsets: ['latin'] })
+
+export const HeroSection = ({imageSrc, heading, desc, stats}: HeroSectionProps) => {
+    return (
+      <div className='flex w-full min-w-[1620px] mt-[150px]'>
+        <div className='relative w-[547px] h-[550px] flex-shrink-0'>
+          <Image src={imageSrc} layout='fill' objectFit='cover' alt='heroSection' />
+        </div>
+        <div className='flex flex-col justify-between h-[550px] ml-[113px]'>
+          <h2 className={`${nunito.className} text-[2.75rem] mb-10`}>{heading}</h2>
+          <p className={`${raleway.className} text-[1.263rem]`}>
+            {desc}
+          </p>
+          <div className='flex justify-between mt-auto'>
+            {stats.map((stat, index) => (
+              <div key={index} className='flex flex-col border-l-2 pl-5'>
+                <span className='text-[5.625rem]'>{stat.value}</span>
+                <span className='text-[1.563rem]'>{stat.label}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Banner } from "./layouts/Banner";
 import { Features } from "./layouts/Features";
+import { HeroSection } from "./layouts/HeroSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,27 @@ export default function Home() {
       <Banner />
       <div className="mt-10">
         <Features />
+        <HeroSection
+        imageSrc={"/hero_furniture.jpg"}
+        heading={"Leader of low prices on furniture in North America – more than 6 years on the market"} 
+        desc={`
+                We manufacture high quality and affordable upholstered furniture in North America. 
+                Over the years we have earned the trust of private customers and large companies. 
+                
+                We are increasing production and are ready to offer our products to neighboring countries. We provide a full range of services, including selection of materials and assembly of furniture for your home. Enjoy your shopping!
+        `}
+        stats={[
+          {
+            value: '7', label: 'years on the market' 
+          },
+          {
+            value: '140+', label: 'Cities in NA'
+          },
+          {
+            value: '250+', label: 'Products to choose'
+          }
+        ]}
+        />
       </div>
     </main>
   );
