0368fb7c0f08c5c7a9dc81fabe4cdf85806b1c15
vadimpavlovskiy <vadimpavlovskiy@icloud.com>
2024-07-22 22:18:48 -0600

    Added forgotten files and code

---
+++ backend/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    public function show($id) {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found', 404]);
+        };
+
+        return response()->json($product);
+    }
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json(['message' => 'Youre on main page']);
+});
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/api/products', [ProductController::class, 'index']);
+    Route::get('/api/products/{id}', [ProductController::class, 'show']);
 });
