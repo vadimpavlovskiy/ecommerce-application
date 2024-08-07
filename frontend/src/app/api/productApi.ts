1a920925789149c30dc7e91ecf676b9b5721c6d6
vadimpavlovskiy <vadimpavlovskiy@icloud.com>
2024-08-06 22:58:44 -0600

    Added a .env support in frontend

--- frontend/src/app/api/productApi.ts
+++ frontend/src/app/api/productApi.ts
@@ -2,21 +2,21 @@ import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export const fetchProducts = async () => {
-  setTimeout(async () => {
-    try {
-      const response = await axios.get("http://127.0.0.1:8000/api/products");
-      return response.data;
-    } catch (error) {
-      console.log("====================================");
-      console.log("Error fetching problem", error);
-      console.log("====================================");
-    }
-  }, 5000);
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_SERVER_API_URL}/products`
+    );
+    return response.data;
+  } catch (error) {
+    console.log("====================================");
+    console.log("Error fetching problem", error);
+    console.log("====================================");
+  }
 };
 export const fetchProductBySlug = async (slug: string) => {
   try {
     const response = await axios.get(
-      `http://127.0.0.1:8000/api/products/${slug}`
+      `${process.env.NEXT_PUBLIC_SERVER_API_URL}/products/${slug}`
     );
     return response.data;
   } catch (error) {
